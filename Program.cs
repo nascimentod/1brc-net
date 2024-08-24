be1d79c5cc0cc5139dd0cfcb92b7bc04a8638663
David Nascimento <david.nascimento@kebrasoft.com>
2024-08-24 10:47:09 +0200

    significant perf improvement when using memory mapping

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
 ﻿using BenchmarkDotNet.Attributes;
 using System.Text;
+using System.IO.MemoryMappedFiles;
+using System.Collections.Concurrent;
+using BenchmarkDotNet.Running;
 
 internal class Program
 {
-    private static async Task Main(string[] args)
+    private static void Main(string[] args)
     {
         //BenchmarkRunner.Run<MeasurementCalculator>();
-        await new MeasurementCalculator().CalculateMeasurements();
+        new MeasurementCalculator().CalculateMeasurements();
     }
 }
 
@@ -14,49 +17,96 @@ internal class Program
 public class MeasurementCalculator 
 {
     [Benchmark]
-    public async Task CalculateMeasurements()
+    public void CalculateMeasurements()
     {
-        var fileName = "C:/source/1brc-net/measurements.txt";
+        var fileName = "C:/source/1brc-net/measurements_big.txt";
+        var fileSize = new FileInfo(fileName).Length;
+        var dop = 20;
 
-        var dictionary = new Dictionary<string, Measurement>();
+        var dictionary = new ConcurrentDictionary<string, Measurement>();
 
-        using var fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
-        using var reader = new StreamReader(fs);
-        string? line;
-        while ((line = await reader.ReadLineAsync()) != null)
-        {
-            ProcessLine(dictionary, line);
-        }
+        using var mmf = MemoryMappedFile.CreateFromFile(fileName);
+        ProcessFile(mmf, fileSize, dop, dictionary);
 
         PrintToConsole(dictionary);
     }
 
-    private static void ProcessLine(Dictionary<string, Measurement> dictionary, string? line)
+    private void ProcessFile(MemoryMappedFile mmf, long fileSize, int degreeOfParallelisation, ConcurrentDictionary<string, Measurement> dictionary)
     {
-        var split = line.Split(';');
-        var city = split[0];
-        var value = double.Parse(split[1]);
+        var chunkSize = fileSize / degreeOfParallelisation;
+        var tasks = new Task[degreeOfParallelisation];
 
-        if (dictionary.TryGetValue(split[0], out var measurement))
+        for(var i = 0; i < degreeOfParallelisation; i++)
         {
-            if (measurement.Min > value)
-            {
-                measurement.Min = value;
-            }
-            if (measurement.Max < value)
-            {
-                measurement.Max = value;
-            }
-            measurement.Sum += value;
-            measurement.Count += 1;
+            var start = i * chunkSize;
+            var end = i == degreeOfParallelisation - 1 ? fileSize : (i + 1) * chunkSize;
+
+            tasks[i] = Task.Run(() => ProcessChunk(mmf, start, end, dictionary));
         }
-        else
+
+        Task.WaitAll(tasks);
+    }
+
+    private void ProcessChunk(MemoryMappedFile mmf, long start, long end, ConcurrentDictionary<string, Measurement> dictionary)
+    {
+        using var stream = mmf.CreateViewStream(start, end - start);
+        using var reader = new BinaryReader(stream);
+        var sb = new StringBuilder();
+
+        try
         {
-            dictionary.Add(split[0], new Measurement() { Min = value, Sum = value, Max = value, Count = 1 });
+            while(true)
+            {
+                var character = reader.ReadChar();
+
+                if(character == '\n')
+                {
+                    ProcessLine(dictionary, sb.ToString());
+                    sb.Clear();   
+                }
+                else
+                {
+                    sb.Append(character);
+                }
+            }
+        }
+        catch(EndOfStreamException)
+        {
+            // END
         }
     }
 
-    private static void PrintToConsole(Dictionary<string, Measurement> dictionary)
+    private static void ProcessLine(ConcurrentDictionary<string, Measurement> dictionary, string line)
+    {
+        try
+        {
+            var split = line.Split(';');
+            var value = double.Parse(split[1]);
+
+            dictionary.AddOrUpdate(split[0], new Measurement() { Min = value, Sum = value, Max = value, Count = 1 }, (_, measurement) =>
+            {
+                if (measurement.Min > value)
+                {
+                    measurement.Min = value;
+                }
+                if (measurement.Max < value)
+                {
+                    measurement.Max = value;
+                }
+                measurement.Sum += value;
+                measurement.Count += 1;
+
+                return measurement;
+            });
+        }
+        catch(Exception ex)
+        {
+            // Ignore for now, but have to set chunk limit at line ends
+            //Console.WriteLine($"{line} was the problem");
+        }
+    }
+
+    private static void PrintToConsole(ConcurrentDictionary<string, Measurement> dictionary)
     {
         var orderedCities = dictionary.OrderBy(x => x.Key).ToDictionary();
 
--- README.md
+++ README.md
@@ -3,8 +3,8 @@ Attempt at the 1 Billion Row Challenge, in C#.
 
 Original idea: https://github.com/gunnarmorling/1brc
 
-Current best, on my home PC (AMD Ryzen 9 7900X, 64GB DDR5 RAM @ 4800 MT/s), as measured by Measure-Command: 
+Current best, on my home PC (AMD Ryzen 9 7900X, 64GB DDR5 RAM @ 4800 MT/s), as measured by Measure-Command ( run 'Measure-Command { dotnet run -c Release }' in this folder): 
 
-Minutes           : 2
-Seconds           : 5
-Milliseconds      : 696
+Minutes           : 0
+Seconds           : 49
+Milliseconds      : 885
