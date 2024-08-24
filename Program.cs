c3702c4e84fb447b1475d7f69fdee0f8a57aa1c1
David Nascimento <david.nascimento@kebrasoft.com>
2024-08-24 09:59:52 +0200

    minor cleanup

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
 ﻿using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Running;
-using System.Globalization;
 using System.Text;
 
 internal class Program
@@ -27,48 +25,58 @@ public class MeasurementCalculator
         string? line;
         while ((line = await reader.ReadLineAsync()) != null)
         {
-            var split = line.Split(';');
-            var city = split[0];
-            var value = double.Parse(split[1]);
-            
-            if(dictionary.TryGetValue(split[0], out var measurement)) 
-            {
-                if(measurement.Min > value)
-                {
-                    measurement.Min = value;
-                }
-                if(measurement.Max < value)
-                {
-                    measurement.Max = value;
-                }
-                measurement.Sum += value;
-                measurement.Count += 1;
-            }
-            else
-            {
-                dictionary.Add(split[0], new Measurement(){Min = value, Sum = value, Max = value, Count = 1});
-            }
+            ProcessLine(dictionary, line);
         }
 
+        PrintToConsole(dictionary);
+    }
+
+    private static void ProcessLine(Dictionary<string, Measurement> dictionary, string? line)
+    {
+        var split = line.Split(';');
+        var city = split[0];
+        var value = double.Parse(split[1]);
+
+        if (dictionary.TryGetValue(split[0], out var measurement))
+        {
+            if (measurement.Min > value)
+            {
+                measurement.Min = value;
+            }
+            if (measurement.Max < value)
+            {
+                measurement.Max = value;
+            }
+            measurement.Sum += value;
+            measurement.Count += 1;
+        }
+        else
+        {
+            dictionary.Add(split[0], new Measurement() { Min = value, Sum = value, Max = value, Count = 1 });
+        }
+    }
+
+    private static void PrintToConsole(Dictionary<string, Measurement> dictionary)
+    {
         var orderedCities = dictionary.OrderBy(x => x.Key).ToDictionary();
 
         var sb = new StringBuilder();
         var i = 0;
-        
-        sb.Append("{");
-        foreach(var kv in orderedCities)
+
+        sb.Append('{');
+        foreach (var kv in orderedCities)
         {
-            if(i < orderedCities.Count - 1) 
+            if (i < orderedCities.Count - 1)
             {
-                sb.Append($"{kv.Key}={kv.Value.Min}/{kv.Value.Sum/kv.Value.Count:F1}/{kv.Value.Max},");
+                sb.Append($"{kv.Key}={kv.Value.Min}/{kv.Value.Sum / kv.Value.Count:F1}/{kv.Value.Max},");
             }
             else
             {
-                sb.Append($"{kv.Key}={kv.Value.Min}/{kv.Value.Sum/kv.Value.Count:F1}/{kv.Value.Max}");
+                sb.Append($"{kv.Key}={kv.Value.Min}/{kv.Value.Sum / kv.Value.Count:F1}/{kv.Value.Max}");
             }
             i++;
         }
-        sb.Append("}");
+        sb.Append('}');
 
         Console.WriteLine(sb.ToString());
     }
